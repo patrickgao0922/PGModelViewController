22083929c07683d3462b4039de9d47409676ade2
Patrick Gao <patrickgao0922@gmail.com>
2018-02-13 12:49:43 +1100

    init commit

---
+++ Example/Pods/Target Support Files/PGModelViewController/PGModelViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PGModelViewController : NSObject
+@end
+@implementation PodsDummy_PGModelViewController
+@end
---
+++ Example/Pods/Target Support Files/Pods-PGModelViewController_Example/Pods-PGModelViewController_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PGModelViewController_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PGModelViewController_Example
+@end
